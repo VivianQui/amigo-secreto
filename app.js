8b51efa873f0f2a9e84042802e86d88a59a9f00c
VivianQui <viloqui@hotmail.com>
2025-02-09 14:44:48 -0500

    actualiza el app.js

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ let listaDeAmigos = [];
 //Función para agregar amigo en la lista
 function agregarAmigo() {
     //Capturar el valor del campo de entrada
+    let inputAmigo = document.getElementById("amigo");
+    let nombre = inputAmigo.value.trim()
+
+    
+
+
 }
